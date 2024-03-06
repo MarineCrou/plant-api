8387689380a1bb8ae0c9ab9babc044e13ee07d87
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-03-06 17:34:15 +0100

    updating main with additional files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 console.log(import.meta.env.VITE_API_KEY);
+const fetch = require("node-fetch");
 
 function App() {
   return <h1>Hello world</h1>;
-  const fetch = require("node-fetch");
 
   (async () => {
     const response = await fetch(
